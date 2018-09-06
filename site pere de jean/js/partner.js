7cfb449073dfaa302fbdbd89933a9ccfdd7e5bd4
mathieu gelbon <33491036+mggb@users.noreply.github.com>
2018-09-06 08:40:09 +0200

    'add

--- site pere de jean/js/partner.js
+++ site pere de jean/js/partner.js
@@ -1,6 +1,8 @@
 var containerpartner = document.querySelector('.container-partner');
-var containchoice = document.querySelectorAll('.contain-choice');
+
 var test = document.querySelector('.container-sort')
+var sort = document.querySelector('.sort')
+var containchoice  = document.querySelector('.container-choice-partner')
 lolo=0;
 
 
@@ -21,7 +23,38 @@ function partner() {
 
 }
 
-
-
     partner()
-    console.log('yolo');
+
+sort.addEventListener('click',function () {
+  // container.innerHTML = '';
+  // console.log('yolo');
+    for (var i = 0; i < data2.partner.length; i++) {
+      console.log('data2.partner[i].categorie');
+      if (data2.partner[i].categorie == containchoice.value  ) {
+        var addpartner = "<li class='contain-partner'>";
+         addpartner += "  <a href='";
+         addpartner += data2.partner[i].linkimg;
+         addpartner += "'>" ;
+         addpartner += "<img class='img-partner' src='./assets/image/Logos/";
+         addpartner += data2.partner[i].img;
+         addpartner += "' alt=''>"
+         addpartner += "</a>"
+         addpartner += "</li>"
+         console.log(data2.partner[i].linkimg);
+         containerpartner.innerHTML += addpartner;
+        }
+
+  }
+})
+
+
+var menu = document.querySelector('.menu');
+var close = document.querySelector('.close')
+var openmenu = document.querySelector('.test')
+
+menu.addEventListener('click',function () {
+  openmenu.style.display = ' block'
+});
+close.addEventListener('click',function () {
+  openmenu.style.display = ''
+});
